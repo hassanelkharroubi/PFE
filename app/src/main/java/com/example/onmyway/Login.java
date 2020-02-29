60bc9e61b4c3e3802aa2a741d6fb8f52458eaad0
Hassan <hassanelkharroubi1997@gmail.com>
2020-02-29 21:26:50 +0100

    add of 2 files toolbar.xml in menu folder and toolbar_layout.xml finaly we get a toolbar menu instead of default appbar

--- app/src/main/java/com/example/onmyway/Login.java
+++ app/src/main/java/com/example/onmyway/Login.java
@@ -1,14 +1,42 @@
 package com.example.onmyway;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.Toolbar;
+
 public class Login extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    //get toolbar_layout
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.toolbar,menu);
+
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/example/onmyway/MainActivity.java
+++ app/src/main/java/com/example/onmyway/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.onmyway;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
+import android.widget.Toolbar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +16,12 @@ public class MainActivity extends AppCompatActivity {
     Runnable runnable;
     ImageView img;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         img = findViewById(R.id.img);
          img.animate().alpha(4000).setDuration(0);
