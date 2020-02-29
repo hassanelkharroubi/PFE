bed8d95426bc6da0f70bae68cfc39d8b9b86ecd8
Hassan <hassanelkharroubi1997@gmail.com>
2020-02-29 19:26:36 +0100

    j'ai termine login UI

---
+++ app/src/main/java/com/example/onmyway/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.onmyway;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
+    Runnable runnable;
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.img);
+         img.animate().alpha(4000).setDuration(0);
+
+         handler = new Handler();
+         handler.postDelayed(new Runnable() {
+             @Override
+             public void run()
+             {
+                 Intent dsp = new Intent(MainActivity.this,Login.class);
+                 startActivity(dsp);
+                 finish();
+             }
+                                             },1000);
+    }
+}
