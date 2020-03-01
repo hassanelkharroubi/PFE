d20c97a0f66bba1ecb2c37894e5bdf1a37616e74
Hassan <hassanelkharroubi1997@gmail.com>
2020-03-01 14:11:16 +0100

    add some Firebase auth API and we config login btn and register btn for add new user and we change tab of list menu if we are in the register activity we have to remove "ajouter" item

--- app/src/main/java/com/example/onmyway/Login.java
+++ app/src/main/java/com/example/onmyway/Login.java
@@ -1,15 +1,17 @@
 package com.example.onmyway;
 
 import android.content.Intent;
-import android.graphics.Color;
+
 import android.os.Build;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
+
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,9 +19,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private FirebaseAuth mAuth;
+    private static final String TAG="login";
+
+    private String email;
+    private String password;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -29,9 +46,17 @@ public class Login extends AppCompatActivity {
     //get toolbar_layout
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail=findViewById(R.id.email);
+        editTextPassword=findViewById(R.id.password);
+
 
     }
-
+    //we don't need here onCreateOptionMenu method
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -40,10 +65,38 @@ public class Login extends AppCompatActivity {
 
 
 
+
         return super.onCreateOptionsMenu(menu);
     }
 
-    public void toLogin(View view) {
+    public void login(View view) {
+        email=editTextEmail.getText().toString();
+        password=editTextPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(Login.this, "Authentication success",
+                                    Toast.LENGTH_SHORT).show();
+                          //  FirebaseUser user = mAuth.getCurrentUser();
+                          //  updateUI(user);
+                        }
+                        else
+                            {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                         //   updateUI(null);
+                          }
+
+                    }
+                });
 
 
     }
@@ -51,11 +104,12 @@ public class Login extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if(item.getItemId()==R.id.ajouter)
+        if(item.getItemId()==android.R.id.home)
         {
-            startActivity(new Intent(this,RegisterActivity.class));
-
+            onBackPressed();
         }
+        if(item.getItemId()==R.id.ajouter)
+            startActivity(new Intent(this,RegisterActivity.class));
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/example/onmyway/RegisterActivity.java
+++ app/src/main/java/com/example/onmyway/RegisterActivity.java
@@ -1,14 +1,129 @@
 package com.example.onmyway;
 
+import android.content.Intent;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private FirebaseAuth mAuth;
+    private static final String TAG="register";
+
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextConfirmPassword;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //get toolbar_layout
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle(getResources().getString(R.string.driver));
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail=findViewById(R.id.email);
+        editTextPassword=findViewById(R.id.password);
+        editTextConfirmPassword=findViewById(R.id.confirmpassword);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.toolbar,menu);
+        menu.removeItem(R.id.ajouter);
+
+
+
+        return super.onCreateOptionsMenu(menu);
+}
+
+    public void regiter(View view) {
+
+        email=editTextEmail.getText().toString().trim();
+        password=editTextPassword.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(RegisterActivity.this, "Authentication success.",Toast.LENGTH_SHORT).show();
+
+                                    //FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        }
+                        else
+                            {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                            }
+
+                        // ...
+                    }
+                });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==android.R.id.home)
+        {
+            onBackPressed();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+    */
+
 }
